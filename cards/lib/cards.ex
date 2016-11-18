50f919dfca195d1d8ea0f405b1929fbc37f8aad4
allansene <allanseneoliveira@gmail.com>
2016-11-18 02:48:42 -0200

    loading test

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,8 +1,31 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
+
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, data} = File.read(filename)
+    :erlang.binary_to_term data
   end
 end
